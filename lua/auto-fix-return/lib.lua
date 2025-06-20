8cc71dca12f79e2469af51d467e1d496905a1bdb
Jay Madden <25359755+Jay-Madden@users.noreply.github.com>
2025-06-19 21:33:40 -0500

    feat: refactored into function parser file

--- lua/auto-fix-return/lib.lua
+++ lua/auto-fix-return/lib.lua
@@ -1,3 +1,5 @@
+local _function = require("auto-fix-return.parsers.function")
+
 local M = {}
 
 local command_id = 0
@@ -36,118 +38,6 @@ function M.disable_autocomds()
 
   vim.api.nvim_del_autocmd(command_id)
   command_id = 0
-end
-
----@class TextGrid
----@field start_row number
----@field start_col number
----@field end_row number
----@field end_col number
-
--- Parse function declarations and return the definition grid if applicable
----@return TextGrid?
-function M.parse_function()
-  local query_str = [[
-    [
-      ;; The following code 
-      ;;
-      ;; func Foo() i, 
-      ;;
-      ;; type foo
-      ;;
-      ;; parses with the error token ABOVE the function declaration 
-      ;; so we handle that in a seperate match
-      (ERROR
-        (function_declaration
-          _?
-          (ERROR)? @error_start
-          result: (_) @result
-          (ERROR)? @error_end
-          body: (_)? @body
-        ) @func
-      ) @outside_error_start
-      (
-        (function_declaration
-          _?
-          (ERROR)? @error_start
-          result: (_) @result
-          (ERROR)? @error_end
-          body: (_)? @body
-        ) @func
-        
-        ;; The following definition `func Foo() i,|` parses
-        ;; with the final error token for the , outside of the function_declaration
-        (ERROR)? @outside_error_end
-      )
-    ]
-  ]]
-
-  local cursor_row, _ = unpack(vim.api.nvim_win_get_cursor(0))
-  -- cursor coordinates need to be converted from row native 1 indexed to 0 indexed for treesitter
-  cursor_row = cursor_row - 1
-  local query = vim.treesitter.query.parse("go", query_str)
-
-  local tree = vim.treesitter.get_parser(0):parse(false)[1]
-
-  local final_start_col, final_end_col = 0, 0
-
-  -- Find the initial func capture so we can bail out
-  -- if the cursor is not on the same row as a `func`
-  -- 
-  -- We need to know upfront if a given parse is even valid to be fixed 
-  -- as we dont know the order we will detect the matches in the match iterator
-  local found = false
-  for id, node, _, _ in query:iter_captures(tree:root(), 0) do
-    local capture_name = query.captures[id]
-    if capture_name == "func" then
-      local start_row, _, _, _= node:range()
-      if cursor_row == start_row then
-        found = true
-        break
-      end
-    end
-  end
-
-  if not found then
-    return nil
-  end
-
-  for id, node, _, _ in query:iter_captures(tree:root(), 0) do
-    local capture_name = query.captures[id]
-    local start_row, start_col, end_row, end_col = node:range()
-
-    -- We only care about captures that are on the same row as the cursor 
-    -- as multiline returns are tricky to parse correctly
-    if cursor_row ~= start_row or cursor_row ~= end_row then
-      goto continue
-    end
-
-    if capture_name == "error_start" then
-      final_start_col = start_col
-    elseif capture_name == "result" then
-      -- As result is the middle result if either start or end
-      -- are already set we should do nothing
-      if final_start_col == 0 then
-        final_start_col = start_col
-      end
-
-      if final_end_col == 0 then
-        final_end_col = end_col
-      end
-    -- The parse tree for `func Foo() int,|` contains the ERROR object OUTSIDE the function_declaration
-    -- which contains the final trailing comma so we match this here to extend our match to include the typed comma
-    elseif capture_name == "error_end" or capture_name == "outside_error_start" or capture_name == "outside_error_end" then
-      final_end_col = end_col
-    end
-
-    ::continue::
-  end
-  return {
-    start_row = cursor_row,
-    end_row = cursor_row,
-    start_col = final_start_col,
-    end_col = final_end_col,
-  }
 end
 
 ---Validate if a given fix is syntactically valid by testing it in a scratch buffer first
@@ -236,9 +126,7 @@ function M.parse_return()
   -- ]]
   --
   -- local query = vim.treesitter.query.parse("go", query_str)
-  local cursor_row, cursor_col = unpack(vim.api.nvim_win_get_cursor(0))
-  -- cursor coordinates need to be converted from row native 1 indexed to 0 indexed for treesitter
-  cursor_row = cursor_row - 1
+  local _, cursor_col = unpack(vim.api.nvim_win_get_cursor(0))
   --
   -- -- We make sure to call the entire parse again to make sure we have the most up to date tree
   -- -- NOTE: without this the bugs are a bit nasty
@@ -275,7 +163,7 @@ function M.parse_return()
   --   ::continue::
   -- end
 
-  local return_def_coords = M.parse_function()
+  local return_def_coords = _function.parse_function()
 
   if return_def_coords == nil then
     return
