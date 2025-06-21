e2cc67c191d9cbba5e2ee5a06aa141796eae5a60
Jay Madden <25359755+Jay-Madden@users.noreply.github.com>
2025-06-21 02:22:52 -0500

    chore: disable more file ops for ci

--- scripts/minimal_init.lua
+++ scripts/minimal_init.lua
@@ -2,8 +2,11 @@ vim.opt.rtp:append(".")
 vim.opt.rtp:append("./testbin/plenary.nvim/")
 vim.opt.rtp:append("./testbin/")
 
--- Disable swap so ci does not fail randomly
+-- Disable swap and other file operations for CI
 vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.writebackup = false
+vim.opt.undofile = false
 
 local plenary_files = vim.fn.globpath(vim.o.rtp, "plugin/plenary.vim", false, true)
 for _, file in ipairs(plenary_files) do
