09dfadcc483bbc17022897c59dc5c0d36639fe51
Jay Madden <25359755+Jay-Madden@users.noreply.github.com>
2025-06-21 02:19:54 -0500

    chore: disable swap files in test init

--- scripts/minimal_init.lua
+++ scripts/minimal_init.lua
@@ -2,6 +2,9 @@ vim.opt.rtp:append(".")
 vim.opt.rtp:append("./testbin/plenary.nvim/")
 vim.opt.rtp:append("./testbin/")
 
+-- Disable swap so ci does not fail randomly
+vim.opt.swapfile = false
+
 local plenary_files = vim.fn.globpath(vim.o.rtp, "plugin/plenary.vim", false, true)
 for _, file in ipairs(plenary_files) do
   vim.cmd.source(file)
